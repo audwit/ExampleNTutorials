55aa28d71aab758589d26513befd4da571471db0
audwit <audwit100@gmail.com>
2016-08-03 16:37:38 -0500

    Writing linked list code

---
+++ ExampleNTutorials/src/ListElement.java
@@ -0,0 +1,20 @@
+
+public class ListElement<T>
+{
+	private ListElement<T> next;
+	private T value;
+	
+	public ListElement (T data)
+	{
+		data = value;
+	}
+	
+	public 
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
